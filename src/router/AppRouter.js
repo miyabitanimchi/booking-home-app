59d471292f5ad1c003c2c7a21c7ea35a21596003
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-06-24 14:38:39 -0700

    add firebaseConfig

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,17 +4,21 @@ import MainPage from "../components/mainpage/MainPage";
 import Detail from "../components/detail/Detail";
 import Checkout from "../components/checkout/Checkout";
 import Footer from "../components/footer/Footer";
+import { auth } from "../firebase/firebase";
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route path="/" component={MainPage} exact={true} />
-      <Route path="/detail" component={Detail} />
-      <Route path="/checkout" component={Checkout} />
-    </Switch>
-    <Footer />
-  </BrowserRouter>
-);
+const AppRouter = () => {
+  console.log(auth);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" component={MainPage} exact={true} />
+        <Route path="/detail" component={Detail} />
+        <Route path="/checkout" component={Checkout} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default AppRouter;
