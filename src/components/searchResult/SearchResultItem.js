df24730d726b37717389cb9c718fcdd6d0044bf0
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-07-04 22:52:53 -0700

    change layout on search result item

--- src/components/searchResult/SearchResultItem.js
+++ src/components/searchResult/SearchResultItem.js
@@ -1,18 +1,10 @@
 import React from "react";
 import "./SearchResultItem.css"
 import { Link } from "react-router-dom";
+import { GoLocation } from 'react-icons/go';
 
 
 const SearchResultItem = (props) => {
-
-  // {
-  //   optimizedThumbUrls,
-  //     name,
-  //     neighbourhood,
-  //     guestReviews,
-  //     ratePlan
-  // }
-
 
   return (
     <Link to={"/detail/" + props.id}>
@@ -20,9 +12,15 @@ const SearchResultItem = (props) => {
         <img className="search-item-img" src={props.optimizedThumbUrls && props.optimizedThumbUrls.srpDesktop} alt={props.name} />
         <div className="search-description-wrap">
           <h3 className="hotel-name">{props.name}</h3>
-          <p>{props.neighbourhood}</p>
-          <p className="review-badge">{props.guestReviews ? props.guestReviews.badge : "-"}</p>
-          <p>Rating: {props.guestReviews ? props.guestReviews.rating : "-"}</p>
+          <p className="neighbourhood">{props.neighbourhood}</p>
+
+          <p className="address">
+            <GoLocation />
+            {props.address.streetAddress && props.address.streetAddress}, {""}
+            {props.address.locality && props.address.locality}, {""}
+            {props.address.region && props.address.region}
+          </p>
+          <p className="rating"><span className="review-badge">{props.guestReviews ? props.guestReviews.badge : "-"}</span>Rating: {props.guestReviews ? props.guestReviews.rating : "-"}</p>
           <p className="search-price"><span className="price">{props.ratePlan ? props.ratePlan.price.current : "-"}</span> / per night</p>
         </div>
       </div>
